6801bc08dc39e6e436b7a8ff9d794616742e96f9
sawatkins <sw@sawatkins.com>
2024-09-11 17:15:33 -0700

    add consumer thread

--- web-scraper/main.py
+++ web-scraper/main.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import queue
 import time
 import json
 import sqlite3 
@@ -9,7 +10,9 @@ from trafilatura.sitemaps import sitemap_search
 from trafilatura.downloads import add_to_compressed_dict, load_download_buffer, buffered_downloads
 from trafilatura.readability_lxml import is_probably_readerable
 from urllib.robotparser import RobotFileParser
-
+import threading
+import requests
+from urllib.error import URLError
 
 def read_urls(file_path):
     with open(file_path, 'r') as file:
@@ -50,24 +53,59 @@ def save_all_posts_to_db(pages, conn):
     print(f"Saved {len(pages)} pages to the database")
     
 def check_robots_txt(domain):
-    robot_parser = RobotFileParser(f"{domain}/robots.txt")
-    robot_parser.read()
-    # TODO: return true if no robots.txt is found
-    return robot_parser.can_fetch("*", domain)
+    robot_parser = RobotFileParser()
+    try:
+        robot_parser.set_url(f"{domain}/robots.txt")
+        robot_parser.read()
+        can_crawl = robot_parser.can_fetch("*", domain)
+        if not can_crawl:
+            print(f"should not crawl {domain}")
+        return can_crawl
+    except (URLError, requests.RequestException) as e:
+        return True
+    except Exception as e:
+        print(f"error when checking robots.txt for {domain}: {type(e).__name__}.")
+        return False
+
+def consumer_thread(extracted_content, db_name, stop_event):
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+    while not stop_event.is_set() or not extracted_content.empty():
+        try:
+            page = extracted_content.get(timeout=1)
+            cursor.execute('''
+            INSERT OR REPLACE INTO pages (title, url, fingerprint, date, text, scraped_on_date)
+            VALUES (?, ?, ?, ?, ?, ?)
+            ''', (page['title'], page['url'], page['fingerprint'], page['date'], page['text'], page['scraped_on_date']))
+            conn.commit()
+            extracted_content.task_done()
+        except queue.Empty:
+            continue
+    conn.close()
+    print("Consumer thread finished")
 
 def main():
-    feeds = read_urls("small-feeds.txt")
+    feeds = read_urls("feed_urls.txt")
     
+    
+    # read cleaned domains from file
+    cleaned_domains = read_urls("cleaned_domains.txt")
     # get the cleaned domains
-    cleaned_domains = []
-    for url in feeds:
-        feed = parse(url)
-        cleaned_domain = clean_url(feed.feed.link)
-        cleaned_domains.append(cleaned_domain)
+    # cleaned_domains = []
+    # for url in feeds:
+    #     try:
+    #         feed = parse(url)
+    #         cleaned_domain = clean_url(feed.feed.link)
+    #     except:
+    #         print(f"error getting domain from feed: {url}")
+    #         # TODO: keep list of error and failed feeds
+    #         continue
+    #     # TODO: validate that the doamin exists and is valid
+    #     cleaned_domains.append(cleaned_domain)
     print('cleaned_domains', len(cleaned_domains))
     print(cleaned_domains)
     
-    # get allowed domains
+    # get allowed domains from robots.txt
     allowed_domains = []
     for domain in cleaned_domains:
         if check_robots_txt(domain):
@@ -87,9 +125,15 @@ def main():
                          'https://letterstoanewdeveloper.com/2022/03/07/changes-from-bootcamp-to-a-real-dev-job/',
                          'https://jakeseliger.com/2021/02/03/the-effect-of-zoning-restrictions-on-the-life-of-the-artist/']
 
+    db_name = 'posts.db'
+    init_db(db_name)  # Just initialize the database
+    extracted_content = queue.Queue()
+    stop_event = threading.Event()
+    consumer = threading.Thread(target=consumer_thread, args=(extracted_content, db_name, stop_event))
+    consumer.start()
+
     threads = 10
     url_store = add_to_compressed_dict(sitemap_links) # test_sitemap_link
-    extracted_content = []
     while url_store.done is False:
         bufferlist, url_store = load_download_buffer(url_store, sleep_time=5)
         for url, result in buffered_downloads(bufferlist, threads):
@@ -107,23 +151,22 @@ def main():
                             'text': extracted_dict['raw_text'],
                             'scraped_on_date': datetime.now().strftime("%Y-%m-%d")
                         }
-                        extracted_content.append(page)
+                        extracted_content.put(page)
                     else:
                         print('Failed to extract content from', url)
             else:
                 print(f"Failed to download {url}")
 
-    save_posts_to_json(extracted_content, 'extracted_content.json')    
-    conn = init_db()
-    save_all_posts_to_db(extracted_content, conn)
-    conn.close()
+    stop_event.set()
+    consumer.join()
+    print('consumer joined')
 
 if __name__ == "__main__":
-    start_scraping = input("Do you want to start scraping? (yes/no): ")
+    start_scraping = input("Are you sure you want to start scraping? (yes/no): ")
     if start_scraping.lower() != "yes":
         print("Scraping cancelled.")
         exit()
     start_time = time.time()
     main()
     end_time = time.time()
-    print(f"Time taken: {end_time - start_time} seconds")
+    print(f"Time taken: {round(end_time - start_time)} seconds")
