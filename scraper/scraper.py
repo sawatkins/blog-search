f7b535c9d93d2019452b15ac13e4aca923aa4555
sawatkins <sw@sawatkins.com>
2025-02-12 22:55:45 -0800

    being v2 of scraper (wip)

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,4 +1,6 @@
 import requests
+from urllib.parse import urlparse
+import fastfeedparser
 
 class Scraper:
     def __init__(self) -> None:
@@ -14,8 +16,39 @@ class Scraper:
         else:
             raise Exception(f"Failed to download smallweb feeds: {response.status_code}")
         
-        print(smallweb_feeds[:5])
+        #print(smallweb_feeds[:5])
+        return smallweb_feeds[532:537]
+        
+    def get_url_path_components(self, url):        
+        parsed_path = urlparse(url)
+        path_elements = [x for x in parsed_path.path.split('/') if x]
+        return path_elements
 
+    def get_sample_urls(self, feed):
+        try:
+            parsed_feed = fastfeedparser.parse(feed)
+        except:
+            print(f"Failed to parse feed: {feed}")
+            return []
+        urls = []
+        
+        for entry in parsed_feed.entries:
+            urls.append(entry.link)
+        
+        return urls
+            
+        
+        
+    
+     
 if __name__ == "__main__":
+    pass
     scraper = Scraper()
-    scraper.start_initial_scrape()
+    smallweb_feeds = scraper.start_initial_scrape()
+    
+    for feed in smallweb_feeds:
+        print(feed)
+        feed_urls = scraper.get_sample_urls(feed)
+        print(feed_urls)
+        for url in feed_urls:
+            print(scraper.get_url_path_components(url))
