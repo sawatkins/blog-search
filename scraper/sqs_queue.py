d8c3ae36ab0bc610dbe8842c3f05c49629bc156e
sawatkins <siawatkins@gmail.com>
2025-02-23 23:38:53 -0800

    add aws keys init boto3

--- scraper/sqs_queue.py
+++ scraper/sqs_queue.py
@@ -1,11 +1,19 @@
 from time import sleep
 import boto3
+import os
+from dotenv import load_dotenv
 
 class SQSQueue:
     def __init__(self) -> None:
-        self.sqs = boto3.resource('sqs', region_name='us-west-2')
+        load_dotenv()
+        self.sqs = boto3.resource(
+            'sqs',
+            region_name='us-west-2',
+            aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+            aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY')
+        )
         self.queue = self.sqs.get_queue_by_name(QueueName='blogsearch-to-scrape')
-        print("got queue:", self.queue.url)
+        print("queue:", self.queue.url)
     
     def send_message(self, url):
         try:
@@ -14,11 +22,16 @@ class SQSQueue:
             print(f"Error sending message to queue: {str(e)}")
             return None
     
-    def process_message(self):
-        message = self.queue.receive_messages(MaxNumberOfMessages=1)
-        if message:
-            return message[0].body
-        return None
+    def receive_message(self):
+        try:
+            message = self.queue.receive_messages(MaxNumberOfMessages=1)
+            if message:
+                body = message[0].body
+                message[0].delete()
+                return body
+        except Exception as e:
+            print(f"Error receiveing message from queue: {str(e)}")
+            return None
     
 
 if __name__ == "__main__":
@@ -27,5 +40,5 @@ if __name__ == "__main__":
     q.send_message(m)
     print("sent to queue:", m)
     sleep(1)
-    returned_message = q.process_message()
+    returned_message = q.receive_message()
     print(returned_message)
